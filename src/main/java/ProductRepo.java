e4f4adbf7830ead978f9aea54d8c8c6b5f69875e
m-reinisch <michael.reinisch@email.de>
2026-05-08 15:59:49 +0200

    implement removeProduct

--- src/main/java/ProductRepo.java
+++ src/main/java/ProductRepo.java
@@ -11,6 +11,7 @@ public class ProductRepo {
     /** Stores one product
      *
      * @param product to store
+     * @return true = successful, false = failed
      */
     public Boolean storeProduct(Product product){
         if(warehouse.containsKey(product.id())){
@@ -19,6 +20,37 @@ public class ProductRepo {
             warehouse.put(product.id(), product);
             return true;
         }
+    }
+
+    /** Removes one producrt
+     *
+     * @param productId id of the product to be removed
+     * @return true = successful, false = failed
+     */
+    public Boolean removeProduct(Integer productId){
+        if (warehouse.containsKey(productId)){
+            warehouse.remove(productId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Displays one product, if available.
+     *
+     * @param id of the product to be shown
+     * @return formated string
+     */
+    public String productInquiry(Integer id){
+        return String.format("1 Icecream 1.99");
+    }
+
+    /** Displays all products.
+     *
+     * @return formated string
+     */
+    public String productInquiry(){
+        return String.format("");
     }
 
     public Map<Integer, Product> getWarehouse() {
--- src/test/java/ProductRepoTest.java
+++ src/test/java/ProductRepoTest.java
@@ -27,4 +27,50 @@ class ProductRepoTest {
         actual= warehouse.storeProduct(product2);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void removeProduct_shouldBeFase_whenFailed() {
+        ProductRepo warehouse= new ProductRepo();
+        Boolean expected= false;
+        Boolean actual;
+
+        actual= warehouse.removeProduct(Integer.valueOf(1));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeProduct_shouldBeTrue_whenSuccessful() {
+        Product product1= new Product(1, "Icecream", 1.99);
+        ProductRepo warehouse= new ProductRepo();
+        Boolean expected= true;
+        Boolean actual;
+
+        warehouse.storeProduct(product1);
+        actual= warehouse.removeProduct(Integer.valueOf(1));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void productInquiry_shouldBeIcecream_whenSuccessful() {
+        Product product1= new Product(1, "Icecream", 1.99);
+        ProductRepo warehouse= new ProductRepo();
+        String expected= "1 Icecream 1.99";
+        String actual;
+
+        warehouse.storeProduct(product1);
+        actual= warehouse.productInquiry(Integer.valueOf(1));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void productInquiry_shouldBeNoProduct_whenFailed() {
+        //Product product1= new Product(1, "Icecream", 1.99);
+        ProductRepo warehouse= new ProductRepo();
+        String expected= "Product not availabble!";
+        String actual;
+
+        //warehouse.storeProduct(product1);
+        actual= warehouse.productInquiry(Integer.valueOf(1));
+        assertEquals(expected, actual);
+    }
 }
