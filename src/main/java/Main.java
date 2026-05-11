1386b5164e886e89ede4504f9cd13814de513d04
m-reinisch <michael.reinisch@email.de>
2026-05-11 13:11:06 +0200

    refactor main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,9 @@ public class Main {
         Product product1= new Product(1, "Icecream", 1.99);
         Product product2= new Product(2, "Whisky", 19.99);
         List<Product> productList= new ArrayList<>();
-        String expected= "Product not available!";
-        String actual;
 
         productList.add(product1);
         productList.add(product2);
-        Order order= new Order(1, "Mixed Order", "MR", productList);
         shop.customerOrder("MR", productList);
     }
 }
